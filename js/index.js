e39875a8cd26c7faa3252056f9bf8d487cf2892f
Anne Kotecha <anne.kotecha@gmail.com>
2018-02-15 22:55:33 +0000

    Added loop to tidy code.  Container is now of fixed position.

--- js/index.js
+++ js/index.js
@@ -1,209 +1,48 @@
-// https://github.com/airbnb/lottie-web
-// Based on this pen: https://codepen.io/celli/pen/NjvrGW
+const globalSpeed = 1;
 
+const projectData = 
+[
+	{id:'o', file: 'json/logo-o.json', segsIn:null, segsOut:null, anim:null},
+	{id:'l', file: 'json/logo-l.json', segsIn:[0,11], segsOut:[78,88], anim:null},
+	{id:'i', file: 'json/logo-i.json', segsIn:[13,25], segsOut:[90,100], anim:null},
+	{id:'c', file: 'json/logo-c.json', segsIn:[25,39], segsOut:[102,113], anim:null},
+	{id:'e', file: 'json/logo-e.json', segsIn:[41,54], segsOut:[115,127], anim:null},
+	{id:'dot', file: 'json/logo-dot.json', segsIn:[56,69], segsOut:[129,141], anim:null},
 
-var animationWindow = document.querySelector('#animation-window');
-var ele_anim_o = document.querySelector('#anim-o');
-var animData_o = {
-		wrapper: ele_anim_o,
-		animType: 'svg',
-		loop: true,
-		prerender: true,
-		autoplay: false,
-		path: 'json/logo-o.json'
-	};
+]
 
-var anim_o = bodymovin.loadAnimation(animData_o);
-anim_o.setSpeed(0.5);
+for (let i=0; i<projectData.length; i++){
+	let item = projectData[i];
 
-
-var ele_anim_i = document.querySelector('#anim-i');
-var animData_i = {
-		wrapper: ele_anim_i,
-		animType: 'svg',
-		loop: false,
-		prerender: true,
-		autoplay: false,
-		path: 'json/logo-i.json'
-	};
-
-var anim_i = bodymovin.loadAnimation(animData_i);
-anim_i.setSpeed(0.5);
-
-
-var ele_anim_c = document.querySelector('#anim-c');
-var animData_c = {
-		wrapper: ele_anim_c,
+	// Get dom element
+	let ele = document.querySelector(`#anim-${item.id}`);
+	// Load in animation and store anim ref
+	let animData = {
+		wrapper: ele,
 		animType: 'svg',
 		loop: false,
 		prerender: true,
 		autoplay: false,
-		path: 'json/logo-c.json'
+		path: item.file
 	};
+	let animRef = bodymovin.loadAnimation(animData);
+	item.anim = animRef;
 
-var anim_c = bodymovin.loadAnimation(animData_c);
-anim_c.setSpeed(0.5);
+	// Set speed
+	item.anim.setSpeed(globalSpeed);
 
-var ele_anim_e = document.querySelector('#anim-e');
-var animData_e = {
-		wrapper: ele_anim_e,
-		animType: 'svg',
-		loop: false,
-		prerender: true,
-		autoplay: false,
-		path: 'json/logo-e.json'
-	};
+	// Get dom element for hover
+	let ele_hover = document.querySelector(`#hover-${item.id}`);
+	// Add mouseover and mouseout events
+	if (item.segsIn != null){
+		ele_hover.addEventListener('mouseover', function() {
+			item.anim.playSegments(item.segsIn, true);
+		});
+	}
+	if (item.segsOut != null){
+		ele_hover.addEventListener('mouseout', function() {
+			item.anim.playSegments(item.segsOut, true);
+		});
+	}
 
-var anim_e = bodymovin.loadAnimation(animData_e);
-anim_e.setSpeed(0.5);
-
-
-var ele_anim_dot = document.querySelector('#anim-dot');
-var animData_dot = {
-		wrapper: ele_anim_dot,
-		animType: 'svg',
-		loop: false,
-		prerender: true,
-		autoplay: false,
-		path: 'json/logo-dot.json'
-	};
-
-var anim_dot = bodymovin.loadAnimation(animData_dot);
-anim_dot.setSpeed(0.5);
-
-
-var ele_anim_l = document.querySelector('#anim-l');
-var animData_l = {
-		wrapper: ele_anim_l,
-		animType: 'svg',
-		loop: false,
-		prerender: true,
-		autoplay: false,
-		path: 'json/logo-l.json'
-	};
-
-var anim_l = bodymovin.loadAnimation(animData_l);
-anim_l.setSpeed(0.5);
-	
-//---------------------------
-const btnPlay = document.getElementById('play');
-const btnPause = document.getElementById('pause');
-const btnRewind = document.getElementById('rewind');
-const btnPlusY = document.getElementById('plusY');
-const btnMinusY = document.getElementById('minusY');
-const btnColor = document.getElementById('color');
-const btnColorOrig = document.getElementById('colorOrig');
-
-
-
-//---------------------------
-
-
-
-
-btnPlay.addEventListener('click', function() {
-		anim.play();
-		anim.setDirection(1);
-});
-btnPause.addEventListener('click', function() {
-	anim.pause();
-});
-btnRewind.addEventListener('click', function() {
-	anim.setDirection(-1);
-	anim.play();
-});
-btnPlusY.addEventListener('click', function() {
-	TweenMax.to(animationWindow, 1, {y:"-=100",  ease:Circ.easeOut});
-});
-btnMinusY.addEventListener('click', function() {
-TweenMax.to(animationWindow, 1, {y:"+=100",  ease:Circ.easeOut});
-});
-btnColor.addEventListener('click', function() {
-	console.log('button color clicked');
-	TweenMax.to(animationWindow, 0.5, {backgroundColor:"#dddddd"});
-});
-btnColorOrig.addEventListener('click', function() {
-	console.log('button color orig clicked');
-	TweenMax.to(animationWindow, 0.5, {backgroundColor:"#ffffff"});
-});
-
-
-//---------------------------
-// HOVER AREAs
-// ele_anim_l.addEventListener('mouseover', function() {
-// 		anim.playSegments([0,75], true);
-// 		console.log('mouseover');
-// });
-
-// ele_anim_l.addEventListener('mouseout', function() {
-// 		anim.playSegments([75,150], true);
-// 		console.log('mouseout');
-// });
-
-const hover_o = document.getElementById('hover-o');
-const hover_l = document.getElementById('hover-l');
-const hover_i = document.getElementById('hover-i');
-const hover_c = document.getElementById('hover-c');
-const hover_e = document.getElementById('hover-e');
-const hover_dot = document.getElementById('hover-dot');
-
-hover_o.addEventListener('mouseover', function() {
-		//anim_o.playSegments([0,11], true);
-		console.log('mouseover anim_o');
-});
-
-hover_o.addEventListener('mouseout', function() {
-		//anim_o.playSegments([78,88], true);
-		console.log('mouseout anim_o');
-});
-//---------------------------
-hover_l.addEventListener('mouseover', function() {
-		anim_l.playSegments([0,11], true);
-		console.log('mouseover anim_l');
-});
-
-hover_l.addEventListener('mouseout', function() {
-		anim_l.playSegments([78,88], true);
-		console.log('mouseout anim_l');
-});
-
-//---------------------------
-hover_i.addEventListener('mouseover', function() {
-		anim_i.playSegments([13,25], true);
-		console.log('mouseover anim_i');
-});
-
-hover_i.addEventListener('mouseout', function() {
-		anim_i.playSegments([90,100], true);
-		console.log('mouseout anim_i');
-});
-//---------------------------
-hover_c.addEventListener('mouseover', function() {
-		anim_c.playSegments([25,39], true);
-		console.log('mouseover anim_c');
-});
-
-hover_c.addEventListener('mouseout', function() {
-		anim_c.playSegments([102,113], true);
-		console.log('mouseout anim_c');
-});
-//---------------------------
-hover_e.addEventListener('mouseover', function() {
-		anim_e.playSegments([41,54], true);
-		console.log('mouseover anim_e');
-});
-
-hover_e.addEventListener('mouseout', function() {
-		anim_e.playSegments([115,127], true);
-		console.log('mouseout anim_e');
-});
-//---------------------------
-hover_dot.addEventListener('mouseover', function() {
-		anim_dot.playSegments([56,69], true);
-		console.log('mouseover anim_dot');
-});
-
-hover_dot.addEventListener('mouseout', function() {
-		anim_dot.playSegments([129,141], true);
-		console.log('mouseout anim_dot');
-});
+}
